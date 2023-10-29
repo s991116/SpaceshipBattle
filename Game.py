70559824aa4bb3780cc76c3b23ef827071da864c
s991116 <philip.juhl@gmail.com>
2023-10-29 22:31:30 +0100

    Uses Arcade as super class

--- Game.py
+++ Game.py
@@ -1,46 +1,45 @@
 # Basic arcade program
-# Displays a white window with a blue circle in the middle
+# Displays a black window with a triangle in the middle
 
 # Imports
 import arcade
 
 # Constants
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Spaceship Battle"
 RADIUS = 150
 TRIANGLE_SIZE = 10
 
-# Open the window
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+class gameWindow(arcade.Window):
+  def __init__(self):
+    super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.set_background_color(arcade.color.BLACK)
+  
+  def setup(self):
+    pass
+  
+  def on_draw(self):
+    arcade.start_render()
+    # Draw a blue circle
+    triangle_pos_x = SCREEN_WIDTH / 2
+    triangle_pos_y = SCREEN_HEIGHT / 2
 
-# Set the background color
-arcade.set_background_color(arcade.color.WHITE)
+    x1 = triangle_pos_x
+    y1 = triangle_pos_y + 3 * TRIANGLE_SIZE
 
-# Clear the screen and start drawing
-arcade.start_render()
+    x2 = triangle_pos_x - TRIANGLE_SIZE
+    y2 = triangle_pos_y
 
-# Draw a blue circle
-triangle_pos_x = SCREEN_WIDTH / 2
-triangle_pos_y = SCREEN_HEIGHT / 2
-
-x1 = triangle_pos_x
-y1 = triangle_pos_y + 3 * TRIANGLE_SIZE
-
-x2 = triangle_pos_x - TRIANGLE_SIZE
-y2 = triangle_pos_y
-
-x3 = triangle_pos_x + TRIANGLE_SIZE
-y3 = triangle_pos_y
+    x3 = triangle_pos_x + TRIANGLE_SIZE
+    y3 = triangle_pos_y
+    arcade.draw_triangle_outline(x1,y1,x2,y2,x3,y3,arcade.color.ARMY_GREEN, border_width= 5)
 
 
-arcade.draw_triangle_outline(x1,y1,x2,y2,x3,y3,arcade.color.ARMY_GREEN, border_width= 5)
-#arcade.draw_circle_filled(
-#    SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
-#)
+def main():
+  window = gameWindow()
+  window.setup()
+  arcade.run()
 
-# Finish drawing
-arcade.finish_render()
-
-# Display everything
-arcade.run()
+if __name__ == "__main__":
+   main()
